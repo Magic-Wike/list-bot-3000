65815418fda3c06d454454bfe973502ec8629e79
Wike <withrowmd@gmail.com>
2022-12-22 08:43:58 -0500

    Baby bot takes it's first command..that has absolutely nothing to do with lists =)

--- index.js
+++ index.js
@@ -10,6 +10,18 @@ const client = new Client({
     ]
 });
 
+function getRandomQuote() {
+    return fetch('https://zenquotes.io/api/random')
+        .then(res => {
+            // console.log(res);
+            return res.json();
+        })
+        .then(data => {
+            // console.log(data[0]);
+            return data[0]["q"] + "\n- "+data[0]["a"];
+        })
+}
+
 client.on('ready', () => {
     console.log('Ready!');
 });
@@ -18,12 +30,21 @@ client.on('messageCreate', message => {
     let tag = message.member.user.tag;
     let id = message.member.id;
     let displayName = message.member.displayName;
+
+    // let channel = message.channel;
+    // let channelID = message.channelId;
+    // console.log(channel);
+    // console.log(channelID);
+
     // log every message to the console
     console.log(tag + ": " +message.content);
-    // if (message.content.includes("baby")) {
-    //     message.reply('Bitch, I love you.');
-    // }
-    if (message.content === "$")
+    if (message.content.includes("baby")) {
+        message.reply('Bitch, I love you.');
+    }
+    if (message.content === '$quote') {
+        console.log('Quote requested...fetching...')
+        getRandomQuote().then(quote => message.channel.send(quote));
+    }
 })
 
 client.login(token);
